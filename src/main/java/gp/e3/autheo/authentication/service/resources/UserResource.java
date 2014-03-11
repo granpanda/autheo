8a5628bba1217a1d09156ca7e9fcb6b9cbfffa35
Julian Espinel <julianespinel@granpanda.com>
2014-03-11 16:52:40 -0500

    Implement UserResource tests, all of them passing. The authentication module is done.

--- src/main/java/gp/e3/autheo/authentication/service/resources/UserResource.java
+++ src/main/java/gp/e3/autheo/authentication/service/resources/UserResource.java
@@ -5,6 +5,7 @@ import gp.e3.autheo.authentication.domain.business.UserBusiness;
 import gp.e3.autheo.authentication.domain.entities.Token;
 import gp.e3.autheo.authentication.domain.entities.User;
 import gp.e3.autheo.authentication.domain.exceptions.TokenGenerationException;
+import gp.e3.autheo.authentication.infrastructure.validators.StringValidator;
 import gp.e3.autheo.authentication.persistence.exceptions.DuplicateIdException;
 
 import java.util.List;
@@ -25,89 +26,146 @@ import javax.ws.rs.core.Response;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
-	
+
 	private final UserBusiness userBusiness;
 	private final TokenBusiness tokenBusiness;
 
 	public UserResource(UserBusiness userBusiness, TokenBusiness tokenBusiness) {
-		
+
 		this.userBusiness = userBusiness;
 		this.tokenBusiness = tokenBusiness;
 	}
-	
+
+	private Response getInvalidSyntaxResponse() {
+
+		String errorMessage = "The request cannot be fulfilled due to bad syntax.";
+		return Response.status(400).entity(errorMessage).build();
+	}
+
 	@POST
 	public Response createUser(User newUser) {
-		
+
 		Response response = null;
-		
-		try {
-			
-			userBusiness.createUser(newUser);
-			response = Response.status(201).entity(newUser).build();
-			
-		} catch (DuplicateIdException e) {
-			
-			response = Response.status(500).entity(e.getMessage()).build();
+
+		if (User.isAValidUser(newUser)) {
+
+			try {
+
+				User createdUser = userBusiness.createUser(newUser);
+				response = Response.status(201).entity(createdUser).build();
+
+			} catch (DuplicateIdException e) {
+
+				response = Response.status(500).entity(e.getMessage()).build();
+			}
+
+		} else {
+
+			response = getInvalidSyntaxResponse();
 		}
-		
+
 		return response;
 	}
-	
+
 	@POST
 	@Path("/token")
 	public Response authenticateUser(User user) {
-		
+
+		Response response = null;
+
+		if ((user != null) && StringValidator.isValidString(user.getUsername()) && 
+				StringValidator.isValidString(user.getPassword())) {
+
+			try {
+
+				if (userBusiness.authenticateUser(user.getUsername(), user.getPassword())) {
+
+					Token token = tokenBusiness.generateToken(user);
+					response = Response.status(201).entity(token.getTokenValue()).build();
+
+				} else {
+
+					String errorMessage = "The user credentials are not valid.";
+					response = Response.status(401).entity(errorMessage).build();
+				}
+
+			} catch (AuthenticationException | TokenGenerationException e) {
+
+				response = Response.status(401).entity(e.getMessage()).build();
+			}
+
+		} else {
+
+			response = getInvalidSyntaxResponse();
+		}
+
+		return response;
+	}
+
+	@GET
+	@Path("/{ username }")
+	public Response getUserByUsername(@PathParam("username") String username) {
+
+		Response response = null;
+
+		if (StringValidator.isValidString(username)) {
+
+			User user = userBusiness.getUserByUsername(username);
+			response = Response.status(200).entity(user).build();
+
+		} else {
+
+			response = getInvalidSyntaxResponse();
+		}
+
+		return response;
+	}
+
+	@GET
+	public Response getAllUsers() {
+
+		List<User> users = userBusiness.getAllUsers();
+		return Response.status(200).entity(users).build();
+	}
+
+	@PUT
+	@Path("/{ username }")
+	public Response updateUser(@PathParam("username") String username, User updatedUser) {
+
+		boolean updatedUserIsValid = (updatedUser != null) && StringValidator.isValidString(updatedUser.getName()) && 
+				StringValidator.isValidString(updatedUser.getPassword());
+
 		Response response = null;
 		
-		try {
+		if (StringValidator.isValidString(username) && updatedUserIsValid) {
+
+			userBusiness.updateUser(username, updatedUser);
+			response = Response.status(200).build();
+
+		} else {
 			
-			if (userBusiness.authenticateUser(user.getUsername(), user.getPassword())) {
-				
-				Token token = tokenBusiness.generateToken(user);
-				response = Response.status(201).entity(token.getTokenValue()).build();
-				
-			} else {
-				
-				String errorMessage = "The user credentials are not valid.";
-				response = Response.status(401).entity(errorMessage).build();
-			}
-			
-		} catch (AuthenticationException | TokenGenerationException e) {
-			
-			response = Response.status(401).entity(e.getMessage()).build();
+			response = getInvalidSyntaxResponse();
 		}
 		
 		return response;
 	}
-	
-	@GET
-	@Path("/{ username }")
-	public Response getUserByUsername(@PathParam("username") String username) {
-		
-		User user = userBusiness.getUserByUsername(username);
-		return Response.status(200).entity(user).build();
-	}
-	
-	@GET
-	public Response getAllUsers() {
-		
-		List<User> users = userBusiness.getAllUsers();
-		return Response.status(200).entity(users).build();
-	}
-	
-	@PUT
-	@Path("/{ username }")
-	public Response updateUser(@PathParam("username") String username, User updatedUser) {
-		
-		userBusiness.updateUser(username, updatedUser);
-		return Response.status(200).build();
-	}
-	
+
 	@DELETE
 	@Path("/{ username }")
 	public Response deleteUser(@PathParam("username") String username) {
 		
-		userBusiness.deleteUser(username);
-		return Response.status(200).build();
+		Response response = null;
+		
+		if (StringValidator.isValidString(username)) {
+			
+			userBusiness.deleteUser(username);
+			response = Response.status(200).build();
+			
+		} else {
+			
+			response = getInvalidSyntaxResponse();
+		}
+		
+		return response;
 	}
 }
