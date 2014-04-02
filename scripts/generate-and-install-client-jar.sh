25dbb72b83328dc7ac3accfb3ae9dd6caa2dde74
Julian Espinel <julianespinel@granpanda.com>
2014-04-02 17:12:01 -0500

    Fix generate and install client jar script.

--- scripts/generate-and-install-client-jar.sh
+++ scripts/generate-and-install-client-jar.sh
@@ -1,4 +1,5 @@
 cd ..
+mkdir dist
 jar cf dist/autheoClient.jar src/main/java/gp/e3/autheo/client/
 cd dist/
 mvn install:install-file -Dfile=autheoClient.jar -DgroupId=gp.e3.autheo.client -DartifactId=client -Dversion=0.1 -Dpackaging=jar
