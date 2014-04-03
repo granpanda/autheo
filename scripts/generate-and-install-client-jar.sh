d6b469a211546a04c343586ce2f4f3458b1c60d4
Julian Espinel <julianespinel@granpanda.com>
2014-04-02 19:11:37 -0500

    Fix bug generating and installing the jar file.

--- scripts/generate-and-install-client-jar.sh
+++ scripts/generate-and-install-client-jar.sh
@@ -1,5 +1,6 @@
 cd ..
 mkdir dist
-jar cf dist/autheoClient.jar src/main/java/gp/e3/autheo/client/
-cd dist/
+cd target/classes/
+jar cf ../../dist/autheoClient.jar gp/e3/autheo/client/*
+cd ../../dist/
 mvn install:install-file -Dfile=autheoClient.jar -DgroupId=gp.e3.autheo.client -DartifactId=client -Dversion=0.1 -Dpackaging=jar
