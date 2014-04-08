69581f06b550e7e9c18e7197ae33f367e30f1623
alejandroosorio <alejandroosorio@granpanda.com>
2014-04-08 11:52:48 -0500

    Added the script for windows to create the AutheoClient.jar and install it on the maven repository.

---
+++ scripts/generate-and-install-client-jar.bat
@@ -0,0 +1,6 @@
+cd..
+mkdir dist
+cd target/classes/
+jar cf ../../dist/autheoClient.jar gp/e3/autheo/client/*
+cd ../../dist/
+mvn install:install-file -Dfile=autheoClient.jar -DgroupId=gp.e3.autheo.client -DartifactId=client -Dversion=0.1 -Dpackaging=jar
