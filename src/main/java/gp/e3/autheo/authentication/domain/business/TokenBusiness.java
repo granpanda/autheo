9b540aff6f3826e2fe704b4387acdefbfd858f7c
Julian Espinel <julianespinel@granpanda.com>
2014-05-29 11:02:45 -0500

    Fix db connection close bug.

--- src/main/java/gp/e3/autheo/authentication/domain/business/TokenBusiness.java
+++ src/main/java/gp/e3/autheo/authentication/domain/business/TokenBusiness.java
@@ -4,6 +4,7 @@ import gp.e3.autheo.authentication.domain.business.constants.TokenTypes;
 import gp.e3.autheo.authentication.domain.entities.Token;
 import gp.e3.autheo.authentication.domain.entities.User;
 import gp.e3.autheo.authentication.domain.exceptions.TokenGenerationException;
+import gp.e3.autheo.authentication.infrastructure.utils.SqlUtils;
 import gp.e3.autheo.authentication.infrastructure.validators.StringValidator;
 import gp.e3.autheo.authentication.persistence.daos.TokenCacheDAO;
 import gp.e3.autheo.authentication.persistence.daos.TokenDAO;
@@ -27,9 +28,9 @@ public class TokenBusiness {
 		List<Token> tokens = tokenDAO.getAllTokens(dbConnection);
 
 		for (Token tokenFromDb : tokens) {
-			
+
 			tokenCacheDao.addTokenUsingTokenValueAsKey(tokenFromDb);
-				
+
 			if (tokenFromDb.getTokenType() == TokenTypes.INTERNAL_API_TOKEN_TYPE.getTypeNumber()) {
 				tokenCacheDao.addTokenUsingOrganizationAsKey(tokenFromDb);
 			}
@@ -42,13 +43,18 @@ public class TokenBusiness {
 		this.tokenDAO = tokenDAO;
 		this.tokenCacheDao = tokenCacheDao;
 
+		Connection dbConnection = null;
+
 		try {
-			Connection dbConnection = dataSource.getConnection();
+			dbConnection = dataSource.getConnection();
 			this.tokenDAO.createTokensTableIfNotExists(dbConnection);
 			updateTokensCache(dbConnection);
-			dbConnection.close();
-		} catch (SQLException e) {
+			
+		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+
+			SqlUtils.closeDbConnection(dbConnection);
 		}
 	}
 
@@ -80,27 +86,33 @@ public class TokenBusiness {
 
 			if (user.isApiClient()) {
 
+				Connection dbConnection = null;
+
 				try {
-					
-					Connection dbConnection = dataSource.getConnection();
-					
+
+					dbConnection = dataSource.getConnection();
+
 					// Generate api token
 					Token apiToken = generateRandomTokenFromUserInfo(user, TokenTypes.API_KEY_TOKEN_TYPE.getTypeNumber());
 					tokenDAO.createToken(dbConnection, apiToken);
 					tokenCacheDao.addTokenUsingTokenValueAsKey(apiToken);
-					
+
 					// Generate internal token
 					Token internalToken = generateRandomTokenFromUserInfo(user, INTERNAL_API_CLIENT_ROLE, TokenTypes.INTERNAL_API_TOKEN_TYPE.getTypeNumber());
 					tokenDAO.createToken(dbConnection, internalToken);
 					tokenCacheDao.addTokenUsingTokenValueAsKey(internalToken);
 					tokenCacheDao.addTokenUsingOrganizationAsKey(internalToken);
-					
+
 					dbConnection.close();
-					
-				} catch (SQLException e) {
+
+				} catch (Exception e) {
 
 					// Enters here if the organization already has a token into the db.
 					e.printStackTrace();
+
+				} finally {
+
+					SqlUtils.closeDbConnection(dbConnection);
 				}
 			}
 
@@ -122,18 +134,24 @@ public class TokenBusiness {
 			token = tokenCacheDao.getTokenByTokenValue(tokenValue);
 
 			if (token == null) {
+				
+				Connection dbConnection = null;
 
 				try {
 
-					Connection dbConnection = dataSource.getConnection();
+					dbConnection = dataSource.getConnection();
 					updateTokensCache(dbConnection);
 					dbConnection.close();
-					
+
 					token = tokenCacheDao.getTokenByTokenValue(tokenValue);
 
 				} catch (SQLException e) {
 
 					e.printStackTrace();
+					
+				} finally {
+					
+					SqlUtils.closeDbConnection(dbConnection);
 				}
 			}
 		}
---
+++ src/main/java/gp/e3/autheo/authentication/infrastructure/utils/SqlUtils.java
@@ -0,0 +1,43 @@
+package gp.e3.autheo.authentication.infrastructure.utils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SqlUtils {
+
+	public static long getGeneratedIdFromResultSet(ResultSet resultSet) throws SQLException {
+
+		long orderId = -1;
+
+		while (resultSet != null && resultSet.next()) {
+			orderId = resultSet.getLong(1);
+		}
+
+		return orderId;
+	}
+
+	public static void rollbackTransaction(Connection dbConnection) {
+
+		if (dbConnection != null) {
+
+			try {
+				dbConnection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public static void closeDbConnection(Connection dbConnection) {
+
+		if (dbConnection != null) {
+
+			try {
+				dbConnection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
