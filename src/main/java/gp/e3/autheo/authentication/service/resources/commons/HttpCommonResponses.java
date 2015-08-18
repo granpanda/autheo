1a7b5cd5fbe3f894d0e0c83733ac7800d69ce06a
Julian Espinel <julianespinel@gmail.com>
2015-08-18 15:46:56 -0500

    Improve error message format.

--- src/main/java/gp/e3/autheo/authentication/service/resources/commons/HttpCommonResponses.java
+++ src/main/java/gp/e3/autheo/authentication/service/resources/commons/HttpCommonResponses.java
@@ -3,12 +3,13 @@ package gp.e3.autheo.authentication.service.resources.commons;
 import gp.e3.autheo.authentication.service.resources.UserResource;
 
 import javax.ws.rs.core.Response;
+import java.util.Map;
 
 public class HttpCommonResponses {
 	
 	public static Response getInvalidSyntaxResponse() {
 
-		String errorMessage = UserResource.getStringInJsonFormat("The request cannot be fulfilled due to bad syntax. Please check the URL and the payload of the HTTP request.");
+		Map<String, String> errorMessage = UserResource.getStringInJsonFormat("error", "The request cannot be fulfilled due to bad syntax. Please check the URL and the payload of the HTTP request.");
 		return Response.status(400).entity(errorMessage).build();
 	}
 }
