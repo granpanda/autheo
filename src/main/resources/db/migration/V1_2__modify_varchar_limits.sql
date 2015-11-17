0c584c7afc4bf623028c84b71ab6379d9de3f759
Julian Espinel <julianespinel@gmail.com>
2015-11-16 19:38:30 -0500

    Increase varchar lenght in DB.

--- src/main/resources/db/migration/V1_2__modify_varchar_limits.sql
+++ src/main/resources/db/migration/V1_2__modify_varchar_limits.sql
@@ -1,3 +1,4 @@
 ALTER TABLE users MODIFY COLUMN name varchar(64), MODIFY COLUMN username varchar(64), MODIFY COLUMN organization_id varchar(64);
 ALTER TABLE tokens MODIFY COLUMN username varchar(64), MODIFY COLUMN organization_id varchar(64);
 ALTER TABLE roles_users MODIFY COLUMN username varchar(64);
+ALTER TABLE permissions MODIFY COLUMN name varchar(128);
