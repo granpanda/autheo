b5052f4ee6967b485ea5e6b97d7ed7d16be603c3
Julian Espinel <julianespinel@gmail.com>
2015-11-16 19:20:55 -0500

    Increase the username varchar limit in all tables.

--- src/main/resources/db/migration/V1_2__modify_varchar_limits.sql
+++ src/main/resources/db/migration/V1_2__modify_varchar_limits.sql
@@ -1,1 +1,3 @@
 ALTER TABLE users MODIFY COLUMN name varchar(64), MODIFY COLUMN username varchar(64), MODIFY COLUMN organization_id varchar(64);
+ALTER TABLE tokens MODIFY COLUMN username varchar(64), MODIFY COLUMN organization_id varchar(64);
+ALTER TABLE roles_users MODIFY COLUMN username varchar(64);
